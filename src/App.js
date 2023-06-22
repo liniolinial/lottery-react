024be8c3a7a5b512fedaa7fa4a5e6ce6669b3a62
Lynn Kim <lynn.kim@brandung.de>
2023-06-22 09:07:15 +0200

    copy local into git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Lottery from "./components/Lottery";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
+    </div>
+  );
+}
+
+export default App;
